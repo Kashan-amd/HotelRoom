adb86626ae4110fb6d5db92e26a4a9ad1b9fe715
test <test@ucp.com>
2024-01-29 12:17:22 +0500

    implemented feature/bookRoom

---
+++ HotelRoom.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Room {
+    string type;
+    int price;
+    int availability;
+};
+
+class Hotel {
+    public:
+        string name;
+        string location;
+        vector<Room> rooms;
+
+        void add_room(Room room) {}
+
+        void display_rooms() {}
+        void book_room() {}
+};
+
+int main{
+
+	Room single_room = {"Single", 50, 10};
+    Room double_room = {"Double", 75, 5};
+
+    Hotel hotel;
+    hotel.name = "My Hotel";
+    hotel.location = "Somewhere, ABC";
+    hotel.add_room(single_room);
+    hotel.add_room(double_room);
+
+    cout << "Welcome to " << hotel.name << " in " << hotel.location << "!" << endl;
+    cout << endl;
+
+    cout << "Available Rooms:" << endl;
+    hotel.display_rooms();
+    hotel.book_room();
+
+    return 0;
+
+}
